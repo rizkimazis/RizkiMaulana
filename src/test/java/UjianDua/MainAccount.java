a9f7552f0ac4de262cf9fd0b7eaf4d4177c9154b
Rizki Maulana Azis <rizkimazis12@gmail.com>
2022-10-11 14:56:43 +0700

    Tugas Minggu 2

---
+++ src/test/java/UjianDua/MainAccount.java
@@ -0,0 +1,31 @@
+package UjianDua;
+
+public class Main {
+    public class MainAccount {
+
+        Account account;
+
+        Account account;
+
+
+        @BeforeMethod
+        public void setUp(){
+            account = new Account();
+        }
+
+        @Test
+        public void insert(){
+            account.insert();
+            account.name(Rizki Maulana);
+            account.amount(1000);
+        }
+
+        @Test
+        public void deposit(){
+            employee.deposit(20000");
+            employee.name(Rizki Maulana);
+            employee.amount(10);
+
+        }
+
+    }
